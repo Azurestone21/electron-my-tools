790ae8d55cf6c61f42a840a2ce360b3da57abaef
azure <1055380813@qq.com>
2026-01-29 22:16:57 +0800

    feat: 音频组件

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,51 +1,12 @@
 <script setup lang="ts">
-import { ElConfigProvider } from 'element-plus'
+import { onMounted } from 'vue'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import { useMusicStore } from './store/modules/music'
-import { storeToRefs } from 'pinia'
-import { initMainProcessSchedules } from './utils/schedules'
-import { watchEffect } from 'vue'
 const locale = zhCn
-const musicStore = useMusicStore()
-const { playingSong, currentTime } = storeToRefs(musicStore)
+import { ElConfigProvider } from 'element-plus'
+import AppAudio from './components/AppAudio.vue'
+import { initMainProcessSchedules } from './utils/schedules'
 
-// 音频当前播放时间
-const timeupdate = (e) => {
-  musicStore.setStore({
-    currentTime: e.target.currentTime || 0
-  })
-}
-
-// 获取歌词并发送到桌面歌词
-const getLyric = async () => {
-  if (playingSong.value.lyric) {
-    try {
-      const lyricArr = await window.musicApi.getLyric(playingSong.value.lyric) || []
-      
-      // 发送歌词数据到桌面歌词
-      await window.musicApi.sendLyricData(
-        JSON.parse(JSON.stringify(lyricArr)),
-        Number(musicStore.currentTime),
-        Boolean(musicStore.isVideoPlay)
-      )
-    } catch (error) {
-      console.error('获取歌词失败:', error)
-    }
-  }
-}
-
-// 监听播放状态变化，持续发送数据到桌面歌词
-watchEffect(() => {
-  if (playingSong.value.lyric) {
-    getLyric()
-  }
-  if (currentTime.value !== undefined) {
-    // 发送播放状态到桌面歌词
-    window.musicApi.sendPlayStatus(currentTime.value, musicStore.isVideoPlay)
-  }
-})
-
-onMounted(() => {
+onMounted(async () => {
   // 监听主进程发送的通知
   initMainProcessSchedules()
 })
@@ -59,10 +20,7 @@ onMounted(() => {
     <router-view v-if="!$route.meta.keepAlive" />
   </el-config-provider>
 
-  <audio id="myAudio" @timeupdate="timeupdate">
-    <source :src="playingSong.songURL" type="audio/mpeg" />
-    您的浏览器不支持 audio 元素。
-  </audio>
+  <app-audio></app-audio>
 </template>
 
 <style>
---
+++ src/renderer/src/components/AppAudio.vue
@@ -0,0 +1,70 @@
+<!-- 音频组件 -->
+<script setup lang="ts">
+import { watchEffect, onMounted, ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useMusicStore } from '../store/modules/music'
+const musicStore = useMusicStore()
+const { playingSong, currentTime } = storeToRefs(musicStore)
+import { getVolume } from '@renderer/utils/music/volume'
+
+let myAudio = ref<HTMLAudioElement | null>(null)
+
+// 音频当前播放时间
+const timeupdate = (e) => {
+  musicStore.setStore({
+    currentTime: e.target.currentTime || 0
+  })
+}
+
+// 获取歌词并发送到桌面歌词
+const getLyric = async () => {
+  if (playingSong.value.lyric) {
+    try {
+      const lyricArr = (await window.musicApi.getLyric(playingSong.value.lyric)) || []
+
+      // 发送歌词数据到桌面歌词
+      await window.musicApi.sendLyricData(
+        JSON.parse(JSON.stringify(lyricArr)),
+        Number(musicStore.currentTime),
+        Boolean(musicStore.isVideoPlay)
+      )
+    } catch (error) {
+      console.error('获取歌词失败:', error)
+    }
+  }
+}
+
+// 监听播放状态变化，持续发送数据到桌面歌词
+watchEffect(() => {
+  if (playingSong.value.lyric) {
+    getLyric()
+  }
+  if (currentTime.value !== undefined) {
+    // 发送播放状态到桌面歌词
+    window.musicApi.sendPlayStatus(currentTime.value, musicStore.isVideoPlay)
+  }
+})
+
+// 监听音量变化
+watchEffect(() => {
+  if (myAudio.value) {
+    myAudio.value.volume = getVolume()
+  }
+})
+
+onMounted(async () => {
+  // 获取音频元素
+  myAudio.value = document.getElementById('myAudio') as HTMLAudioElement
+})
+</script>
+
+<template>
+  <div>
+    <audio id="myAudio" @timeupdate="timeupdate">
+      <source :src="playingSong.songURL" type="audio/mpeg" />
+      您的浏览器不支持 audio 元素。
+    </audio>
+  </div>
+</template>
+
+<style lang="less" scoped></style>
