5a762b64ac3d75e535a3894be460b0861b570d13
azure <1055380813@qq.com>
2026-02-17 17:33:21 +0800

    refactor: 别名

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -15,7 +15,8 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@share': resolve('src/share')
       }
     },
     plugins: [
