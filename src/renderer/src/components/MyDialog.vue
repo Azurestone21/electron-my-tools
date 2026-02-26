46365d893f63be45d32f38d6146471f637256394
azure <1055380813@qq.com>
2026-02-26 23:45:13 +0800

    feat: 新增弹窗组件

---
+++ src/renderer/src/components/MyDialog.vue
@@ -0,0 +1,56 @@
+<!-- 音乐设置 -->
+<script setup lang="ts">
+const props = defineProps({
+  open: {
+    type: Boolean,
+    default: false
+  },
+  title: {
+    type: String,
+    default: ''
+  }
+})
+const emits = defineEmits(['onCancel'])
+
+const cancelClick = () => {
+  emits('onCancel')
+}
+</script>
+
+<template>
+  <el-dialog :modelValue="open" :show-close="false" :width="'60%'">
+    <template #header>
+      <div class="dialog-header">
+        <div>{{ title }}</div>
+        <div class="close-btn" @click="cancelClick">
+          <el-icon><Close /></el-icon>
+        </div>
+      </div>
+    </template>
+    <template #default>
+      <div class="dialog-content">
+        <slot></slot>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<style lang="less" scoped>
+.dialog-header {
+  font-size: 16px;
+  font-weight: 600;
+  color: var(--foreground);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .close-btn {
+    cursor: pointer;
+  }
+}
+.dialog-content {
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+}
+</style>
